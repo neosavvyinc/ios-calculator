a068eae2fb6814df2d8efa94e88f37b091110546
Adam Parrish <waparrish@Adam-Parrishs-MacBook-Pro.local>
2012-01-18 22:26:08 -0500

    Initial Commit

---
+++ Calculator/CalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorAppDelegate.h
+//  Calculator
+//
+//  Created by Adam Parrish on 1/18/12.
+//  Copyright (c) 2012 Neosavvy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Adam Parrish on 1/18/12.
+//  Copyright (c) 2012 Neosavvy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
