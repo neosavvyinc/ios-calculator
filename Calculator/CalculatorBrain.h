a66d32bb49292073aafaae852fe77a486b6ebab8
Adam Parrish <waparrish@Adam-Parrishs-MacBook-Pro.local>
2012-01-18 23:09:22 -0500

    Defining the base of the Calculator.

---
+++ Calculator/CalculatorBrain.h
@@ -0,0 +1,16 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by Adam Parrish on 1/18/12.
+//  Copyright (c) 2012 Neosavvy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+
+- (void)pushOperand: (double)operand;
+- (double)performOperation: (NSString *)operation;
+
+@end
